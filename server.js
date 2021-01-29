923a1fede9074b1e84d2b203802755f34d58d328
vikash452 <vikashp0901@gmail.com>
2021-01-29 12:59:43 +0530

    client side established

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app=express();
 require('dotenv').config();
 const PORT = 3000 || process.env.PORT;
 
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/public/index.html')
+})
+
 app.listen(PORT,()=>{
     console.log('server started on port 3000')
 })
