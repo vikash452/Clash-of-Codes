3140966b8033102d6422c87650ddfd844ed0a9a2
vikash452 <vikashp0901@gmail.com>
2021-04-25 11:47:37 +0530

    division page logic

--- client/src/screens/Division.js
+++ client/src/screens/Division.js
@@ -1,5 +1,7 @@
 import {useState,useEffect} from 'react'
 import BlobbyButton from './BlobbyButton'
+import M from 'materialize-css'
+import user_uniq_subms_cf from './user_uniq_subms_cf'
 
 function Division()
 {
@@ -7,13 +9,19 @@ function Division()
     var [div1,setDiv1]=useState([])
     var [div2,setDiv2]=useState([])
     var [div3,setDiv3]=useState([])
+    var [div4,setDiv4]=useState([])
     var [edRound,setEdRound]=useState([])
     var [globalRound,setGlobalRound]=useState([])
     var [toDisplay,setDisplay]=useState([])
     var [contestType,setContestType]=useState('notselected')
+    var [unattempted,setUnattempted]=useState(false)
+    var [handle,setHandle]=useState(null)
 
     useEffect(()=>{
-        // GetAllContests()
+        var tooltip_elem=document.querySelectorAll('.tooltipped')
+        M.Tooltip.init(tooltip_elem)
+        var user=JSON.parse(localStorage.getItem('user'))
+        setHandle(user.codeforces)
     },[])
 
     async function GetAllContests()
@@ -23,6 +31,7 @@ function Division()
         var div1_temp=[]
         var div2_temp=[]
         var div3_temp=[]
+        var div4_temp=[]
         var ed_temp=[]
         var global_temp=[]
 
@@ -40,26 +49,65 @@ function Division()
             div2_temp.push(cont)
             else if(cont.name.search('Div. 3') !=-1 )
             div3_temp.push(cont)
+            else if(cont.name.search('Div. 4') !=-1 )
+            div4_temp.push(cont)
 
         });
 
-        console.log(div1_temp);
-        console.log(div2_temp);
-        console.log(div3_temp);
-        console.log(ed_temp);
-        console.log(global_temp);
-
+        // console.log(div1_temp);
+        // console.log(div2_temp);
+        // console.log(div3_temp);
+        // console.log(div4_temp);
+        // console.log(ed_temp);
+        // console.log(global_temp);
+        var display_temp=[]
         console.log(contestType)
         if(contestType === 'div1')
-        setDisplay(div1_temp)
+        display_temp=[...div1_temp]
+        // setDisplay(div1_temp)
         if(contestType === 'div2')
-        setDisplay(div2_temp)
+        display_temp=[...div2_temp]
+        // setDisplay(div2_temp)
         if(contestType === 'div3')
-        setDisplay(div3_temp)
+        display_temp=[...div3_temp]
+        // setDisplay(div3_temp)
+        if(contestType === 'div4')
+        display_temp=[...div4_temp]
+        // setDisplay(div4_temp)
         if(contestType === 'global')
-        setDisplay(global_temp)
+        display_temp=[...global_temp]
+        // setDisplay(global_temp)
         if(contestType === 'ed')
-        setDisplay(ed_temp)
+        display_temp=[...ed_temp]
+        // setDisplay(ed_temp)
+        console.log(display_temp)
+        console.log('un=',unattempted)
+        if(unattempted)
+        {
+            console.log(handle)
+            var user_accepted_submissions=await user_uniq_subms_cf(handle)
+            var user_attempted_contests=new Set()
+
+            user_accepted_submissions.forEach((value,key)=>{
+                user_attempted_contests.add(value.contestId)
+            })
+            console.log(user_attempted_contests)
+            var temp2=[]
+            display_temp.forEach((item,index)=>{
+                console.log(item.id)
+                if(!user_attempted_contests.has(item.id))
+                {
+                    
+                    temp2.push(item)
+                    // console.log(index)
+                    // display_temp.splice(index,1);
+                }
+            })
+
+            setDisplay(temp2)
+
+        }
+        setDisplay(display_temp)
     }
 
     return (
@@ -69,8 +117,32 @@ function Division()
                 <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='div1'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('div1')}}>Division 1</span>
                 <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='div2'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('div2')}}>Division 2</span>
                 <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='div3'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('div3')}}>Division 3</span>
+                <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='div4'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('div4')}}>Division 4</span>
                 <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='ed'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('ed')}}>Educational Round</span>
                 <span className='chip' style={{cursor:'pointer', backgroundColor:contestType==='global'?'lightgreen':'#e4e4e4', textAlign:'center'}} onClick={()=>{setContestType('global')}}>Global Round</span>
+            </div>
+
+            <div style={{marginTop:'5rem'}}>
+                <label>
+                    <input type='checkbox' checked={unattempted} 
+                    onChange={()=>{
+                        setUnattempted(!unattempted)
+                    }}
+                    />
+                    <span style={{fontSize:'20px', color:'white'}}>
+                        Unattempted 
+                        <i className='material-icons small tooltipped'
+                        data-position='top'
+                        data-tooltip="When selected the questions will be in increasing order. Suppose 
+                        if 1st question is selected to be of 1000 rating then 2nd will be of 1100,
+                        3rd will be of 1100 and so on
+                        "
+                        style={{fontSize:'20px', marginLeft:'20px'}}
+                        >
+                            help_outline
+                        </i>
+                    </span>
+                </label>
             </div>
 
             <div style={{marginTop:'5rem'}}>
