fbb7e0782b61f62ff3d4ca3827b223d57c7d131a
vikash452 <vikashp0901@gmail.com>
2021-05-11 21:08:44 +0530

    home page changes

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -5,6 +5,8 @@ import M from 'materialize-css'
 import './design.css';
 import CF from '../images/cf.jpg'
 import Preloader from '../assets/Preloader'
+import { use } from 'passport';
+import UniqueSubmissions from './user_uniq_subms_cf'
 
 function Home() {
 
@@ -12,17 +14,22 @@ function Home() {
     var [name, setName] = useState('');
     var [upcomingCF, setUpcomingCF] = useState([]);
     var [upcomingCC, setUpcomingCC] = useState([]);
-    var user;
+    var [userCFData,setUserCFData]=useState(null)
+    var [user,setUser]=useState({})
+    var [gotusercf , setGotUserCf]=useState(false)
+    // var user;
     const history = useHistory();
-    useEffect(() => {
+    useEffect(async () => {
 
-        user = JSON.parse(localStorage.getItem('user'))
+        var user_temp = JSON.parse(localStorage.getItem('user'))
+        setUser(user_temp)
+        setGotUserCf(true)
         // console.log(user)
-        if (!user) {
+        if (!user_temp) {
             history.push('/signin')
         }
         else {
-            setName(user.name)
+            setName(user_temp.name)
         }
 
         fetch('/codechef/upcomingContests',{
@@ -58,18 +65,60 @@ function Home() {
                 setUpcomingCF(temp)
         })
 
+        
+
     }, [])
 
-    useEffect(()=>{
-        console.log(upcomingCF)
-    },[upcomingCF])
+    useEffect(async ()=>{
+        console.log(user)
+        if(user && user.codeforces)
+        {
+            var TotalQuestions=await UniqueSubmissions(user.codeforces)
+            var prom=await fetch(`https://codeforces.com/api/user.info?handles=${user.codeforces}`)
+            var temp_data=await prom.json()
+            console.log(typeof(temp_data))
+            var data={
+                ...temp_data.result[0],
+                total:TotalQuestions.size
+            }
+            console.log(data)
+            setUserCFData(data)
+        }
+    },[gotusercf])
 
     
     return (
         <div className="Home-div">
             <div className="msg-div" style={{}}>
                 <span className="home-msg">Hi {name},</span>
-                <p className="msg-content">Let us help you reach the pinnacle of competitive coding. </p>
+                {/* <p className="msg-content">Let us help you reach the pinnacle of competitive coding. </p> */}
+            </div>
+
+            <div>
+                {
+                    console.log(userCFData),
+                    (gotusercf && (userCFData != null))
+                    ?
+                    <>
+                    </>
+                    :
+                    <div
+                    style={{
+                        fontSize:'3rem',
+                        backgroundColor:'rgba(64,64,64,0.6)',
+                        paddingRight:'30px',
+                        paddingLeft:'30px',
+                        borderRadius:'20px'
+                    }}
+                    >
+                        <h2>
+                            Codeforces
+                        </h2>
+                        {user.codeforces}
+                        <br/>
+                        {/* {userCFData.total} on Codeforces */}
+                    </div>    
+                }
             </div>
 
             <div className='upcoming_contest_list_div'>
